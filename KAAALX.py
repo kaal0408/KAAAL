c20f77842bc5c584a0e93bfe4545dc9707c60d14
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-02-10 17:13:15 +0530

    Update and rename LionX.py to KAAALX.py

--- KAAALX.py
+++ KAAALX.py
@@ -6,8 +6,8 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://gitHub.com/TeamLionX/LionX && cd LionX && python3 -m userbot")
+system("git clone https://gitHub.com/kaal0408/KAAAL && cd KAAAL && python3 -m userbot")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Murat_30_God")
 # Script written for stop heroku suspension
 # use with credit else gay
